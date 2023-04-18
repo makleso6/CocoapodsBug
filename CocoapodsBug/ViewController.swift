aa4453a58be637e2da69b72aa91ddf823399dae7
kolesnik <kolesnik@yandex-team.ru>
2023-04-18 18:23:14 +0300

    Initial Commit

---
+++ CocoapodsBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocoapodsBug
+//
+//  Created by Maksim Kolesnik on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
